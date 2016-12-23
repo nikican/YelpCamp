f6593afecdb1ab1f5770d1193209ed1ed9496785
Nikica <novakovicnikica@gmail.com>
2016-12-23 20:58:12 +0000

    Create Comment model and add it to Campground model

---
+++ models/comment.js
@@ -0,0 +1,12 @@
+var mongoose = require("mongoose");
+
+var commentSchema = new mongoose.Schema({
+
+    text: String,
+    author: String
+});
+
+
+var Comment = mongoose.model("Comment", commentSchema);
+
+module.exports = Comment;
