1ac40681910510d014db86abfe9472a6f892ae01
Nikica <novakovicnikica@gmail.com>
2016-12-25 09:57:29 +0000

    Add authentication

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-var bodyParser = require("body-parser"),
+var express = require("express"),
+    bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
     seedDB = require("./seeds"),
     Campground = require("./models/campground"),
     Comment = require("./models/comment"),
-    express = require("express"),
+    User = require("./models/user"),
+    expressSession = require("express-session"),
     app = express();
 
 
@@ -15,6 +19,24 @@ app.use(bodyParser.urlencoded({
 app.use(express.static(__dirname + "/public"));
 seedDB();
 
+//PASSPORT CONFIG
+app.use(expressSession({
+    secret: "Nomi is Russian blue",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+});
+
 app.get("/", function(req, res) {
     res.render("landingPage");
 });
@@ -25,6 +47,10 @@ app.get("/", function(req, res) {
 
 //INDEX
 app.get("/campgrounds", function(req, res) {
+
+    var currentUser = req.user;
+
+    //get campgrounds from DB
     Campground.find({}, function(error, campgrounds) {
         if (!error) {
             console.log(`Campgorunds retrieved: ${campgrounds.length}`)
@@ -39,12 +65,12 @@ app.get("/campgrounds", function(req, res) {
 });
 
 //NEW
-app.get("/campgrounds/new", function(req, res) {
+app.get("/campgrounds/new", isLoggedIn, function(req, res) {
     res.render("campgrounds/new");
 });
 
 //CREATE
-app.post("/campgrounds", function(req, res) {
+app.post("/campgrounds", isLoggedIn, function(req, res) {
     var newCampground = req.body.campground;
 
     Campground.create(newCampground, function(error, campground) {
@@ -79,7 +105,8 @@ app.get("/campgrounds/:id", function(req, res) {
 // COMMENTS ROUTES
 // =======================
 
-app.get("/campgrounds/:id/comments/new", function(req, res) {
+//NEW
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res) {
     var campgroundId = req.params.id;
 
     Campground.findById(campgroundId, function(error, campground) {
@@ -92,11 +119,10 @@ app.get("/campgrounds/:id/comments/new", function(req, res) {
             console.log(error);
         }
     });
-
 });
 
 //CREATE
-app.post("/campgrounds/:id/comments", function(req, res) {
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res) {
     var newComment = req.body.comment;
 
     var campgroundId = req.params.id;
@@ -133,6 +159,62 @@ app.post("/campgrounds/:id/comments", function(req, res) {
         }
     });
 });
+
+// ======================
+// AUTHENTICATION ROUTES
+// ======================
+
+//show register form
+app.get("/register", function(req, res) {
+    res.render("register");
+})
+
+//handle sign up
+app.post("/register", function(req, res) {
+
+    var username = req.body.username;
+    var password = req.body.password;
+
+    var newUser = new User({
+        username: username
+    });
+
+    User.register(newUser, password, function(error, user) {
+        if (!error) {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/campgrounds");
+            });
+        }
+        else {
+            console.log(error);
+            res.render("register");
+        }
+    });
+})
+
+//show log in form
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+//handle login in
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/register"
+}));
+
+//log out
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+});
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
 
 //C9 IP and port
 app.listen(process.env.PORT, process.env.IP, function() {
--- models/user.js
+++ models/user.js
@@ -1,10 +1,12 @@
 var mongoose = require("mongoose");
-var passportLocalMongoose = require("passport-local-mongose");
+var passportLocalMongoose = require("passport-local-mongoose");
 
 var userSchema = new mongoose.Schema({
     username: String,
     password: String
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 var User = mongoose.model("User", userSchema);
 
